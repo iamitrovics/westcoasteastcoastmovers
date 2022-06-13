955a47e0dfe98e653028bb684d99344fdbe92e89
Srdjan Mitrovic <iam@mitrovics.com>
2022-06-14 00:55:58 +0200

    fix serach

--- functions.php
+++ functions.php
@@ -114,17 +114,21 @@ function wpex_parse_request_tricksy( $query ) {
 }
 add_action( 'pre_get_posts', 'wpex_parse_request_tricksy' );
 
-function fb_filter_query( $query, $error = true ) {
+if ( ! is_admin() ) {
 
-    if ( is_search() ) {
-        $query->is_search = false;
-        $query->query_vars['s'] = false;
-        $query->query['s'] = false;
+	function fb_filter_query( $query, $error = true ) {
 
-        if ( $error == true )
-            $query->is_404 = true;
-    }
+		if ( is_search() ) {
+			$query->is_search = false;
+			$query->query_vars['s'] = false;
+			$query->query['s'] = false;
+
+			if ( $error == true )
+				$query->is_404 = true;
+		}
+	}
+
+	add_action( 'parse_query', 'fb_filter_query' );
+	add_filter( 'get_search_form', function() { return null;} );
+
 }
-
-add_action( 'parse_query', 'fb_filter_query' );
-add_filter( 'get_search_form', function() { return null;} );
